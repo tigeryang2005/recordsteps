998045a8296c1ea34d141d362d63d3faa19083b4
yangyin <yangyin@inad.com>
2016-10-08 16:11:07 +0800

    first commit

---
+++ app/src/main/java/com/tiger/example/recordsteps/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tiger.example.recordsteps;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
